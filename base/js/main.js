9994bd7c352f57a2a7e47b45ab5abd178993ce68
Boaz Marube <boazmarube1@gmail.com>
2025-06-26 20:31:17 +0300

    Feat:add header with nav and theme toggle

---
+++ base/js/main.js
@@ -0,0 +1,27 @@
+//theme toggle
+const themeToggle = document.getElementById('theme-toggle')
+    themeToggle.addEventListener("click", ()=>{
+        const htmlElement =document.documentElement;
+        htmlElement.classList.toggle("dark");
+    
+        if(htmlElement.classList.contains("dark")){
+            themeToggle.textContent = "☀️";
+        }else{
+            themeToggle.textContent =" 🌙";
+        }
+    });
+//hambugger display
+document.addEventListener('DOMContentLoaded', initApp)
+const initApp = () => {
+    const hamburgerBtn = document.getElementById('hamburger-button')
+    const mobileMenu = document.getElementById('mobile-menu')
+
+    const toggleMenu = () => {
+        mobileMenu.classList.toggle('hidden')
+        mobileMenu.classList.toggle('flex')
+        hamburgerBtn.classList.toggle('toggle-btn')
+    }
+
+    hamburgerBtn.addEventListener('click', toggleMenu)
+    mobileMenu.addEventListener('click', toggleMenu)
+}
